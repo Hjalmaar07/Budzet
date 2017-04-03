54f582bd4d8f74e9ec8457911abbab509e7f4b4b
Hjalmaar07 <damiank9447@gmail.com>
2017-04-03 15:08:44 +0200

    Register class filled. Full registration and login support. Added debbugable true to build gradle.

--- app/src/main/java/com/example/dkuberski/budzet/MainActivity.java
+++ app/src/main/java/com/example/dkuberski/budzet/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dkuberski.budzet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void goToLogin(View v)
+    {
+        Intent login = new Intent(this, Login.class);
+        startActivity(login);
+    }
+
+
 }

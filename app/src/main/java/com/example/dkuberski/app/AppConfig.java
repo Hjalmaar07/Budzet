6efcbed5c6e3c6508c3c2906f3ce53b91eb765a0
Hjalmaar07 <damiank9447@gmail.com>
2017-04-03 13:27:00 +0200

    Login and register activity added. Login class filled.

---
+++ app/src/main/java/com/example/dkuberski/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.dkuberski.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://217.61.16.10/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://217.61.16.10/android_login_api/register.php";
+}

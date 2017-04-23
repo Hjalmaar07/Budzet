7cf11e3b61ef788e61269cccfad7a5ef84396a02
Hjalmaar07 <damiank9447@gmail.com>
2017-04-23 11:10:31 +0200

    main activity's projectDisplay and spinner added

--- app/src/main/java/com/example/dkuberski/budzet/Project.java
+++ app/src/main/java/com/example/dkuberski/budzet/Project.java
@@ -1,26 +1,14 @@
 package com.example.dkuberski.budzet;
 
-import android.graphics.Path;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.sql.SQLException;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class Project extends AppCompatActivity {
 
@@ -40,21 +28,26 @@ public class Project extends AppCompatActivity {
 
         try {
             // open a connection to the site
-            URL url = new URL("217.61.16.10/mateusz/add_project.php");
+            String sURL = "217.61.16.10/mateusz/add_project.php?";
+            sURL += "nazwaProjektu="+nazwaProjektuTxt +
+                    "&"+
+                    "opisProjektu="+opisProjektuTxt;
+
+            URL url = new URL(sURL);
             URLConnection con = url.openConnection();
             // activate the output
             con.setDoOutput(true);
-            PrintStream ps = new PrintStream(con.getOutputStream());
+            /*PrintStream ps = new PrintStream(con.getOutputStream());
             // send your parameters to your site
             ps.print(nazwaProjektuTxt);
             ps.print(miejsceProjektuTxt);
             ps.print(opisProjektuTxt);
-
+*/
             // we have to get the input stream in order to actually send the request
             con.getInputStream();
 
             // close the print stream
-            ps.close();
+            //ps.close();
         } catch (MalformedURLException e1) {
             e1.printStackTrace();
         } catch (IOException e2) {
